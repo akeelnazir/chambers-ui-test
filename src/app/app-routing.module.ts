9296999ec4d781a06700b79bb8cd286a058ef412
Akeel Nazir <akeel@akeelnazir.com>
2020-06-14 22:24:28 +0100

    Routing with home as default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
+import { HomeComponent } from './components/home/home.component'
+import { PostsComponent } from './components/posts/posts.component'
+import { UsersComponent } from './components/users/users.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'users', component: UsersComponent },
+  { path: '**', redirectTo: 'home' }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
 
 import { MaterialModule } from '../shared/material/material.module'
 
 import { HeaderComponent } from './header/header.component'
-import { LeftMenuComponent } from './left-menu/left-menu.component'
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component'
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    LeftMenuComponent
+    LeftMenuComponent,
+    HomeComponent,
+    PostsComponent,
+    UsersComponent
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent,
